d84661236ea1c7f5a049269d4b0675f269ae06cc
Martyna Obrebska <martyna.obrebska94@gmail.com>
2023-04-21 00:37:07 +0200

    Some styling fixes.

--- Netflix - widok glownego dashboardu (react)/Starter-code/src/modules/HomeModule/components/MainPhoto/MainPhoto.tsx
+++ Netflix - widok glownego dashboardu (react)/Starter-code/src/modules/HomeModule/components/MainPhoto/MainPhoto.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import "./MainPhoto.scss";
-import { MainContent } from "../MainContent/MainContent";
 
 export const MainPhoto: React.FC = () => (
   <div className="home-page-main-photo">
-    <div className="home-page-main-photo-shadow top" />
-    <MainContent />
     <div className="home-page-main-photo-shadow bottom" />
   </div>
 );
