a4ff74935879f055a21b8bbcfa3be857a73ab1a7
Martyna Obrebska <martyna.obrebska94@gmail.com>
2023-04-21 12:03:43 +0200

    Add footer.

---
+++ Netflix - widok glownego dashboardu (react)/Starter-code/src/global-components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.scss";
+
+export const Footer: React.FC = () => {
+  return (
+    <div className="footer">
+      <p>© 2023 | Martyna Obrębska</p>
+    </div>
+  );
+};
