e2b01827907a9829706066cb020ce16d935adaf8
Martyna Obrebska <martyna.obrebska94@gmail.com>
2023-04-21 11:14:02 +0200

    Add main menu transition.

--- Netflix - widok glownego dashboardu (react)/Starter-code/src/global-components/MainMenu/MainMenu.tsx
+++ Netflix - widok glownego dashboardu (react)/Starter-code/src/global-components/MainMenu/MainMenu.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MainMenu.scss";
 import NetflixSvg from "@/assets/images/NETFLIX.svg";
 
-export const MainMenu: React.FC = () => (
-  <div className="mainMenu">
-    <button className="home">
-      <img src={NetflixSvg} className="logo" alt="Netflix logo" />
-    </button>
-  </div>
-);
+export const MainMenu: React.FC = () => {
+  const [activeMenu, setActiveMenu] = useState(false);
+  const [scrollValue, setScrollValue] = useState(0);
+  const menuClassName = `mainMenu${activeMenu ? " active" : ""}`;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const verticalScrollValue = window.scrollY;
+      setActiveMenu(verticalScrollValue > 64);
+      setScrollValue(verticalScrollValue);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollValue]);
+
+  return (
+    <div className={menuClassName}>
+      <button className="home">
+        <img src={NetflixSvg} className="logo" alt="Netflix logo" />
+      </button>
+    </div>
+  );
+};
