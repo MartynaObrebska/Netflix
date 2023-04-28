4793b46fe66e65f673c9899846bb5ef29f722ca5
Martyna Obrebska <martyna.obrebska94@gmail.com>
2023-04-28 22:41:36 +0200

    Update Layout imports.

--- Netflix - widok glownego dashboardu (react)/Starter-code/src/App.tsx
+++ Netflix - widok glownego dashboardu (react)/Starter-code/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { HomePage } from "@modules";
-import { MainMenu } from "@/global-components";
-import { Footer } from "./global-components/Layout/Footer/Footer";
-import { VideoPopUp } from "./global-components/PopUps/VideoPopUp/VideoPopUp";
-import { PreviewPopUp } from "./global-components/PopUps/PreviewPopUp/PreviewPopUp";
+import {
+  MainMenu,
+  Footer,
+  VideoPopUp,
+  PreviewPopUp,
+} from "@/global-components";
 const App: React.FC = () => (
   <>
     <MainMenu />
--- Netflix - widok glownego dashboardu (react)/Starter-code/src/global-components/index.ts
+++ Netflix - widok glownego dashboardu (react)/Starter-code/src/global-components/index.ts
@@ -1,1 +1,4 @@
 export * from "./Layout/MainMenu/MainMenu";
+export * from "./Layout/Footer/Footer";
+export * from "./PopUps/VideoPopUp/VideoPopUp";
+export * from "./PopUps/PreviewPopUp/PreviewPopUp";
