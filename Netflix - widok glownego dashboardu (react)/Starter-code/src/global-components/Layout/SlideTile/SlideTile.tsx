523e920d2a7d0c8af4cde004044ae1cb255fd49a
Martyna Obrebska <martyna.obrebska94@gmail.com>
2023-05-02 22:41:56 +0200

    Add RWD to top10 slide tiles.

--- Netflix - widok glownego dashboardu (react)/Starter-code/src/global-components/Layout/SlideTile/SlideTile.tsx
+++ Netflix - widok glownego dashboardu (react)/Starter-code/src/global-components/Layout/SlideTile/SlideTile.tsx
@@ -10,16 +10,19 @@ type SlideTileProps = {
 
 export const SlideTile = ({ image, top10, rank }: SlideTileProps) => {
   const [active, setActive] = useState(false);
-  const containerClassName = `slide-tile-container${top10 ? " top10" : ""}`;
+  const containerClassName = `container${top10 ? " top10" : ""}`;
+  const slideTileContainerClassName = `slide-tile-container${
+    top10 ? " top10" : ""
+  }`;
   const slideClassName = `slide-tile${top10 ? " top10" : ""}`;
 
   return (
     <div
-      className="container"
+      className={containerClassName}
       onMouseEnter={() => setActive(true)}
       onMouseLeave={() => setActive(false)}
     >
-      <div className={containerClassName}>
+      <div className={slideTileContainerClassName}>
         {top10 && <p className={`slide-tile-rank rank${rank}`}>{rank}</p>}
         <div className={slideClassName}>
           <img className="slide-tile-image" src={image} />
