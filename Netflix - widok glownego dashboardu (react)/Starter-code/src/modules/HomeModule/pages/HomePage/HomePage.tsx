a814333526a8a7bd102fe67236183c4eadbf739a
Martyna Obrebska <martyna.obrebska94@gmail.com>
2023-05-06 22:26:23 +0200

    Add home page disabled class when active preview or video pop up.

--- Netflix - widok glownego dashboardu (react)/Starter-code/src/modules/HomeModule/pages/HomePage/HomePage.tsx
+++ Netflix - widok glownego dashboardu (react)/Starter-code/src/modules/HomeModule/pages/HomePage/HomePage.tsx
@@ -3,9 +3,15 @@ import { MainPhoto } from "../../components/MainPhoto/MainPhoto";
 import "./HomePage.scss";
 import { MainContent } from "../../components/MainContent/MainContent";
 import { Carousels } from "../../components/Carousels/Carousels";
+import { useAppSelector } from "@/app/hooks";
 export const HomePage: React.FC = () => {
+  const activePreview = useAppSelector((state) => state.preview.active);
+  const activeVideo = useAppSelector((state) => state.video.activeVideo);
+  const homePageClassName = `home-page ${
+    activePreview || activeVideo ? "disabled" : ""
+  }`;
   return (
-    <div className="home-page">
+    <div className={homePageClassName}>
       <MainPhoto />
       <MainContent />
       <Carousels />
